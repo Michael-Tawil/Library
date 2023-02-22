a136e5a302c921ada84a2b53df7d0381c34ed537
michael-tawil <michaeltawil3@gmail.com>
2023-02-23 08:48:47 +1100

    integrated input handling

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let myLibrary = [];
 bookcont = document.getElementById("bookcont");
 
-
 function Book(title,author,pages,read){
     this.title = title
     this.author = author
@@ -19,46 +18,60 @@ function addBookToLibrary(title,author,pages,read) {
 
 function shwbook(){
         bookcont.innerHTML = "";
-    myLibrary.forEach(book =>{
-        let bookbtn = document.createElement("button");
-        bookbtn.innerHTML=`${book.read}`;
-        bookbtn.classList.add("nnewbook","yesno");
-        let bookcard = document.createElement("div");
-        bookcard.classList.add("card");
-        bookcard.dataset.index = myLibrary.indexOf(book);
-        let bookcardh = document.createElement("h1");
-        bookcardh.innerText=`${book.title}`;
-        let bookcarddesc = document.createElement("p");
-        bookcarddesc.innerText=`${book.author} \n ${book.pages}`;
+        myLibrary.forEach(book =>{
+            let bookbtn = document.createElement("button");
+            let bookDelbtn = document.createElement("button");
+            let bookcard = document.createElement("div");
+            let bookcardh = document.createElement("h1");
+            let bookcarddesc = document.createElement("p");
 
-        if(book.read === "read"){
-            bookbtn.style.background = "#50C878";
-        }
-        else{
-            bookbtn.style.background = "#ED2939";
-        }
+            bookDelbtn.innerHTML="Delete";
 
-        bookcont.appendChild(bookcard);
-        bookcard.appendChild(bookcardh);
-        bookcard.appendChild(bookcarddesc);
-        bookcard.appendChild(bookbtn);
+            bookbtn.innerHTML=`${book.read}`;
+            bookbtn.classList.add("nnewbook","yesno");
+            bookcard.classList.add("card");
+            bookcard.dataset.index = myLibrary.indexOf(book);
+            bookcardh.innerText=`${book.title}`;
+            bookcarddesc.innerText=`${book.author} \n ${book.pages}`;
+
+            if(book.read === "read"){
+                bookbtn.style.background = "#50C878";
+            }
+            else{
+                bookbtn.style.background = "#ED2939";
+            }
+
+            bookcont.appendChild(bookcard);
+            bookcard.appendChild(bookcardh);
+            bookcard.appendChild(bookcarddesc);
+            bookcard.appendChild(bookbtn);
+            bookcard.appendChild(bookDelbtn);
     })
 }
 
 function delbook(e){
-    let i  = e.target.dataset.index;
+    let i  = e.target.parentNode.dataset.index;
     myLibrary.splice(i,1);
     shwbook();
 }
 function chngread(e){
-    let i = e.target.dataset.index;
+    let i = e.target.parentNode.dataset.index;
     let Selbook = myLibrary.at(i)
     Selbook.read === "read" ? Selbook.read = "not read" :Selbook.read="read"
     shwbook();
+    
+}
+function handleinput(e){
+    if (e.target.innerHTML === "read" || e.target.innerHTML === "not read"){
+        chngread(e);
+    }else if(e.target.innerHTML === "Delete"){
+        delbook(e);
+    }else{
+
+    }
 }
 
 addBookToLibrary("The Albion","DR.T",300,"read");
-addBookToLibrary("The maxi","DR.T",300,"read");
+addBookToLibrary("The maxi","DR.N",300,"read");
 
-console.log(myLibrary);
-window.addEventListener("click",chngread)
+bookcont.addEventListener("click",handleinput);

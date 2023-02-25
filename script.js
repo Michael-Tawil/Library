daf1f73aae5e04ba58ddb76f824c0c6ce8c7ed6d
michael-tawil <michaeltawil3@gmail.com>
2023-02-26 00:14:31 +1100

    Add book feature Addedgit add .

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let myLibrary = [];
-bookcont = document.getElementById("bookcont");
+ let myLibrary = [];
+const bookcont = document.getElementById("bookcont");
+const modal = document.querySelector("#modal");
+const closemodalbtn = document.querySelector("[data-close-button]");
+const ovlay = document.querySelector("#overlay");
+const nbookbtn = document.querySelector("[data-newbook-btn]");
+const bookget = document.querySelector("#submit-nbook");
 
 function Book(title,author,pages,read){
     this.title = title
@@ -16,7 +21,12 @@ function addBookToLibrary(title,author,pages,read) {
     shwbook();
 }
 
+function addbookbtn(){
+
+}
+
 function shwbook(){
+    if(myLibrary.length > 0){
         bookcont.innerHTML = "";
         myLibrary.forEach(book =>{
             let bookbtn = document.createElement("button");
@@ -33,8 +43,9 @@ function shwbook(){
             bookcard.dataset.index = myLibrary.indexOf(book);
             bookcardh.innerText=`${book.title}`;
             bookcarddesc.innerText=`${book.author} \n ${book.pages}`;
+            bookDelbtn.classList.add("nnewbook");
 
-            if(book.read === "read"){
+            if(book.read === "Read"){
                 bookbtn.style.background = "#50C878";
             }
             else{
@@ -46,9 +57,20 @@ function shwbook(){
             bookcard.appendChild(bookcarddesc);
             bookcard.appendChild(bookbtn);
             bookcard.appendChild(bookDelbtn);
-    })
+        })
+    }else{
+        shwmodal()
+    }
 }
-
+function getbook (){
+    let title = document.querySelector("#bookt").value;
+    let author  = document.querySelector("#booka").value;
+    let pages = document.querySelector("#bookp").value;
+    let read = document.querySelector("#bookr").value;
+    modal.classList.remove("active");
+    ovlay.classList.remove("active");
+    addBookToLibrary(title,author,pages,read);
+}
 function delbook(e){
     let i  = e.target.parentNode.dataset.index;
     myLibrary.splice(i,1);
@@ -57,21 +79,34 @@ function delbook(e){
 function chngread(e){
     let i = e.target.parentNode.dataset.index;
     let Selbook = myLibrary.at(i)
-    Selbook.read === "read" ? Selbook.read = "not read" :Selbook.read="read"
+    Selbook.read === "Read" ? Selbook.read = "Not Read" :Selbook.read="Read"
     shwbook();
     
 }
 function handleinput(e){
-    if (e.target.innerHTML === "read" || e.target.innerHTML === "not read"){
+    if (e.target.innerHTML === "Read" || e.target.innerHTML === "Not Read"){
         chngread(e);
     }else if(e.target.innerHTML === "Delete"){
         delbook(e);
-    }else{
-
-    }
+    }else{}
 }
+function shwmodal(){
+    bookcont.innerHTML = "";
+    modal.classList.add("active");
+    ovlay.classList.add("active");
+    
+}
+addBookToLibrary("m","t",3,"Read")
+addBookToLibrary("m","t",3,"Read")
+addBookToLibrary("m","t",3,"Read")
 
-addBookToLibrary("The Albion","DR.T",300,"read");
-addBookToLibrary("The maxi","DR.N",300,"read");
+closemodalbtn.addEventListener('click', ()=>{
+    modal.classList.remove("active");
+    ovlay.classList.remove("active");
+    
+    shwbook();
+})
 
+nbookbtn.addEventListener('click',shwmodal)
 bookcont.addEventListener("click",handleinput);
+bookget.addEventListener('click',getbook)
